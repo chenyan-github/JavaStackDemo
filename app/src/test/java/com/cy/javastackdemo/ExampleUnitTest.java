77b21b3a4c26b4024d208d2b3853ef99e780d3b8
MacBook－chenyan <chenyan2010521@126.com>
2019-05-26 18:14:21 +0800

    commit for modified junit test code.

--- app/src/test/java/com/cy/javastackdemo/ExampleUnitTest.java
+++ app/src/test/java/com/cy/javastackdemo/ExampleUnitTest.java
@@ -5,6 +5,8 @@ import com.cy.javastackdemo.stack.LinkedStack;
 
 import org.junit.Test;
 
+import java.util.logging.Logger;
+
 import static org.junit.Assert.*;
 
 /**
@@ -13,6 +15,9 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
+    private static Logger log = Logger.getLogger(ExampleUnitTest.class.getSimpleName());
+
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
@@ -22,12 +27,15 @@ public class ExampleUnitTest {
     public void testArrayStack() {
         ArrayStack<Integer> arrayStack = new ArrayStack();
         for(int i = 0;i < 100;i ++) {
-            int num = (int) (Math.random() * 100);
-            System.out.println("push: " + i + ", " + arrayStack.push(num));
-        }
-
-        while (!arrayStack.empty()) {
-            System.out.println("pop: " + arrayStack.pop());
+            Integer num = (int) (Math.random() * 100);
+            log.info("random number is: " + num);
+            assertEquals("Push test failure!", num, arrayStack.push(num));
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            assertEquals("Pop test failure!", num, arrayStack.pop());
         }
     }
 
@@ -35,12 +43,15 @@ public class ExampleUnitTest {
     public void testLinkedStack() {
         LinkedStack<Integer> arrayStack = new LinkedStack<>();
         for(int i = 0;i < 100;i ++) {
-            int num = (int) (Math.random() * 100);
-            System.out.println("push: " + i + ", " + arrayStack.push(num));
-        }
-
-        while (!arrayStack.empty()) {
-            System.out.println("pop: " + arrayStack.pop());
+            Integer num = (int) (Math.random() * 100);
+            log.info("random number is: " + num);
+            assertEquals("Push test failure!", num, arrayStack.push(num));
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            assertEquals("Pop test failure!", num, arrayStack.pop());
         }
     }
 }
